b362845abcdee262a7a21be402609df939f2386c
Preeti biswas <preeti.biswas@brovitech.com>
2023-12-01 23:36:17 +0530

    first commit

---
+++ src/components/Message.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Message = () => {
+  return (
+    <div>
+      Message Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem
+      officia delectus quo qui tempora? Cum sapiente eaque, alias eum accusamus
+      reiciendis itaque nostrum nulla! Minus cum quia asperiores! Eius
+      accusantium voluptate nulla, maxime necessitatibus obcaecati perspiciatis
+      temporibus deserunt doloribus magni illo laudantium repellat ratione.
+      Debitis esse architecto impedit veniam reprehenderit.
+    </div>
+  )
+}
+
+export default Message
---
+++ src/main.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+
+const config = {
+  initialColorMode: 'light',
+  useSystemColorMode: false,
+}
+const theme = extendTheme({ config })
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>,
+)
