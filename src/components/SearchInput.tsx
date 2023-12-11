743be06194d22792aedaf2f8be67455a0ee13d37
Preeti biswas <preeti.biswas@brovitech.com>
2023-12-12 01:25:34 +0530

    fix-navbar

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -2,15 +2,20 @@ import { Input, InputGroup, InputLeftElement } from '@chakra-ui/react'
 import { useRef } from 'react'
 import { FcSearch } from 'react-icons/fc'
 import useGameQueryStore from '../store'
+import { useNavigate } from 'react-router-dom'
 
 const SearchInput = () => {
+  const navigate = useNavigate()
   const setSearchText = useGameQueryStore((s) => s.setSearchText)
   const Ref = useRef<HTMLInputElement>(null)
   return (
     <form
       onSubmit={(e) => {
         e.preventDefault()
-        if (Ref.current) setSearchText(Ref.current?.value)
+        if (Ref.current) {
+          setSearchText(Ref.current?.value)
+          navigate('/')
+        }
       }}
     >
       <InputGroup>
